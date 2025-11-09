c6774d4dc3ec61bd2018101b840c649a223881d7
jseiwert <jseiwertiii@gmail.com>
2025-11-09 14:52:00 -0800

    Corrected the comparison operator to >=

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     basic.pause(1000)
-    if (input.temperature() == 25) {
+    if (input.temperature() >= 25) {
         basic.showIcon(IconNames.Sad)
     } else {
         basic.showIcon(IconNames.Happy)
